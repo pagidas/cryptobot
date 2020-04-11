f22c6be3186a1e56ef0f791d25530f8080cc7a35
pagidas <pagakrivos@gmail.com>
2020-04-11 13:32:28 +0100

    Adds a ConfigReader to read port and host from conf file

---
+++ src/main/kotlin/me/pysquad/cryptobot/config/ConfigReader.kt
@@ -0,0 +1,14 @@
+package me.pysquad.cryptobot.config
+
+import com.typesafe.config.ConfigFactory
+
+data class ServerConfig(val host: String, val port: Int)
+data class AppConfig(val server: ServerConfig)
+
+object ConfigReader {
+    private val config = ConfigFactory.load()
+
+    private val app = AppConfig(
+        ServerConfig(host = config.getString("server.host"), port = config.getInt("server.port"))
+    )
+}
