966a2423d88e70753617169368f1e25ac2d85bf8
pagidas <pagakrivos@gmail.com>
2020-04-12 20:07:33 +0100

    Updates open api contract in http4k to validate incoming modeled requests

--- src/main/kotlin/me/pysquad/cryptobot/subscriber/endpoints/SubscribeToMarket.kt
+++ src/main/kotlin/me/pysquad/cryptobot/subscriber/endpoints/SubscribeToMarket.kt
@@ -1,5 +1,8 @@
 package me.pysquad.cryptobot.subscriber.endpoints
 
+import me.pysquad.cryptobot.Channel
+import me.pysquad.cryptobot.CoinbaseMessageType.SUBSCRIBE
+import me.pysquad.cryptobot.ProductId
 import me.pysquad.cryptobot.coinbase.CoinbaseApi
 import me.pysquad.cryptobot.common.Endpoint
 import me.pysquad.cryptobot.subscriber.CoinbaseSubscribeRequest
@@ -11,9 +14,18 @@ import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 
 class SubscribeToMarket(val coinbase: CoinbaseApi): Endpoint {
+    private val exampleSubscribeRequest =
+        CoinbaseSubscribeRequest(
+            type = SUBSCRIBE,
+            productIds = listOf(ProductId("ETH-GBP")),
+            channels = listOf(Channel("ticker"))
+        )
+
     override val contractRoute =
         "/market/subscribe" meta {
             summary = "Subscribes to the real-time market data flow on orders and trades."
+            receiving(CoinbaseSubscribeRequest.lens to exampleSubscribeRequest)
+            returning(OK)
         } bindContract POST to handler()
 
     private fun handler(): HttpHandler = { req: Request ->
