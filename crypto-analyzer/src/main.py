6caddfc0821d8343ace09046958803b84968529f
Nikos G <elpaxoudis@gmail.com>
2020-05-11 00:13:13 +0300

    adds API call for initial product subscription

--- crypto-analyzer/src/main.py
+++ crypto-analyzer/src/main.py
@@ -1,7 +1,11 @@
+from utils.adapter_request_helper import subscribe_products
+
 def main():
     print("==================================")
     print("HELLO!! -- CRYPTO-ANALYZER SERVICE")
     print("==================================")
+
+    subscribe_products(["BTC-EUR"])
 
 
 if __name__ == "__main__":
