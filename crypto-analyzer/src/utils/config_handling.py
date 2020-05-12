758a05212247835be4f8dba195a8eb6875861fa3
Nikos G <elpaxoudis@gmail.com>
2020-05-12 19:27:29 +0300

    fixes a pyYaml package related warning

--- crypto-analyzer/src/utils/config_handling.py
+++ crypto-analyzer/src/utils/config_handling.py
@@ -5,7 +5,7 @@ class ConfigHandler:
 
     def __init__(self):
         try:
-            with open("../resources/analayzer_configuration.yml", "r") as config_file:
-                self.config_data = yaml.load(config_file)
+            with open("./resources/analyzer_configuration.yml", "r") as config_file:
+                self.config_data = yaml.load(config_file, Loader=yaml.Loader)
         except OSError as err:
             print(err)
