8a4f26b7e160770f5674e5840da7f08740ce740c
Nikos G <elpaxoudis@gmail.com>
2020-05-12 21:07:44 +0300

    implements a more generic subscription method

--- crypto-analyzer/src/utils/adapter_request_helper.py
+++ crypto-analyzer/src/utils/adapter_request_helper.py
@@ -1,25 +1,14 @@
 import requests
-HOST = "http://coinbase_adapter:9000"
-subscribe_endpoint = HOST + "/api/market/subscribe"
 
-def subscribe_product(product):
-    body = {"type": "Subscribe",
-            "productIds": [
-                product 
-            ],
-            "channels": [
-                "ticker"
-            ]}
 
-    r = requests.post(subscribe_endpoint, json=body)
-    print(r.text)
+def subscribe_product(handler, *args):
+    if not all(map(lambda product_id: isinstance(product_id, str), args)):
+        raise ValueError
+    
+    subscribe_endpoint = handler.products_subscription_endpoint
+    body = handler.post_request_body_template
 
-def subscribe_products(product_ids):
-    body = {"type": "Subscribe",
-            "productIds": product_ids,
-            "channels": [
-                "ticker"
-            ]}
-
+    body['productIds'] = list(args)
+    
     r = requests.post(subscribe_endpoint, json=body)
     print(r.text)
--- crypto-analyzer/src/utils/config_handling.py
+++ crypto-analyzer/src/utils/config_handling.py
@@ -9,3 +9,26 @@ class ConfigHandler:
                 self.config_data = yaml.load(config_file, Loader=yaml.Loader)
         except OSError as err:
             print(err)
+
+    @property
+    def products_subscription_endpoint(self):
+        if not self.config_data:
+            raise ValueError
+
+        return 'http://{host}:{port}{endpoint}'.format(
+            host = self.config_data['coinbase_adapter']['host'],
+            port = self.config_data['coinbase_adapter']['port'],
+            endpoint = self.config_data['coinbase_adapter']['subscribe_products_endpoint']
+        )
+
+    @property
+    def rethink_host(self):
+        return self.config_data['rethinkdb']['host']
+
+    @property
+    def rethink_port(self):
+        return self.config_data['rethinkdb']['port']
+
+    @property
+    def post_request_body_template(self):
+        return self.config_data['coinbase_adapter']['post_request_body_template']
