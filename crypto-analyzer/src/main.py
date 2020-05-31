5043757da1fba28aa4fd860888c9eec226c28256
esigalas <evsigalas@gmail.com>
2020-05-31 20:28:42 +0800

    testing new function in main

--- crypto-analyzer/src/main.py
+++ crypto-analyzer/src/main.py
@@ -2,7 +2,7 @@ import time
 from utils.adapter_request_helper import subscribe_products
 from utils.db_connection import connect_to_db, print_feed
 from utils.config_handling import ConfigHandler
-
+from utils.anal_utils import get_latest_prices
 
 def main():
     print("==================================")
@@ -13,12 +13,13 @@ def main():
     handler = ConfigHandler()
 
     print("Sleeping for ten seconds...")
-    time.sleep(10)
-    subscribe_products(handler, "BTC-EUR")
+    # time.sleep(10)
+    # subscribe_products(handler, "BTC-EUR")
 
     connect_to_db(handler)
-    print_feed()
-
+    # print_feed()
+    values = get_latest_prices(20)
+    print(values)
 
 if __name__ == "__main__":
     main()
