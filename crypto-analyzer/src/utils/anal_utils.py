7abed4c62458b7bb3eb59c349c9a25efd6a3cbf7
esigalas <evsigalas@gmail.com>
2020-06-07 22:10:27 +0800

    renamed the name of the function that gets the latest prices.

--- crypto-analyzer/src/utils/anal_utils.py
+++ crypto-analyzer/src/utils/anal_utils.py
@@ -2,7 +2,7 @@ import sys
 from rethinkdb import r
 import numpy as np
 
-def get_latest_prices(values_length):
+def get_prices(values_length):
     query = r.db('cryptobot')\
             .table('messages').pluck(['price', 'time', 'trade_id']).order_by(r.desc('time')).limit(values_length).run()
     prices = []
@@ -13,4 +13,5 @@ def get_latest_prices(values_length):
         prices.append(float(d['price']))
         trade_ids.append(int(d['trade_id']))
         time.append(d['time'].strftime('%M:%S'))
+
     return prices[::-1], trade_ids[::-1], time[::-1]
