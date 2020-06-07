2ef5bd9ce51e477ac0e855abbe36d0f899162060
nikosg <ngiachou@gmail.com>
2020-06-07 22:27:28 +0300

    Implements new workflow with authorization

--- crypto-analyzer/src/utils/adapter_request_helper.py
+++ crypto-analyzer/src/utils/adapter_request_helper.py
@@ -1,14 +1,32 @@
 import requests
 
 
+def cb_auth_request(config_handler):
+    body = {
+        "credentials": {
+            "user": config_handler.username,
+            "password": config_handler.password
+        }
+    }
+
+    auth_endpoint = config_handler.auth
+
+    r = requests.post(auth_endpoint, json=body)
+
+    # print(r.json()['authToken'])
+    config_handler.auth_token = r.json()['authToken']
+
+
 def subscribe_products(config_handler, *args):
     if not all(map(lambda product_id: isinstance(product_id, str), args)):
         raise ValueError
 
+    headers = {"Authorization": "Basic {}".format(
+        config_handler.auth_token)}
     subscribe_endpoint = config_handler.products_subscription_endpoint
     body = config_handler.post_request_body_template
 
     body['productIds'] = list(args)
-    
-    r = requests.post(subscribe_endpoint, json=body)
+
+    r = requests.post(subscribe_endpoint, json=body, headers=headers)
     print(r.text)
