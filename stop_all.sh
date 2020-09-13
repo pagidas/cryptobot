307f22938d26b2f927d9eeb3e513d4316f312010
pagidas <pagakrivos@gmail.com>
2020-09-13 16:31:15 +0100

    Inits graphql service in docker-compose

--- stop_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Stopping all services..."
-docker-compose down
-echo "Done! You might need to cleanup intermediate images built.
-                                      They will be under '<none>' in: $ docker images"
