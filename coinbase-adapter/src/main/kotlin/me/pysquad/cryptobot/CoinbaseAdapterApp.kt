465260d4af66ea4098df7600d72e4be9e81eeb6a
pagidas <pagakrivos@gmail.com>
2020-09-25 15:29:21 +0100

    Duplication: Removes duplicate code
    Since we have implemented the graphql standalone app
    and the coinbase-subscriber. Some of the code had to
    be removed.

--- coinbase-adapter/src/main/kotlin/me/pysquad/cryptobot/CoinbaseAdapterApp.kt
+++ coinbase-adapter/src/main/kotlin/me/pysquad/cryptobot/CoinbaseAdapterApp.kt
@@ -1,6 +1,5 @@
 package me.pysquad.cryptobot
 
-import me.pysquad.cryptobot.coinbase.CoinbaseApi
 import me.pysquad.cryptobot.common.Http4kApp
 import me.pysquad.cryptobot.config.getDbConfig
 import me.pysquad.cryptobot.config.getServerPort
@@ -10,7 +9,6 @@ fun buildApp() = object: Http4kApp {
     override val port = getServerPort
     override val routes = CoinbaseAdapterRoutes(
             CoinbaseAdapterService.buildIt(),
-            CoinbaseApi.buildIt(),
             SecurityProvider.buildIt()
     )
 }
