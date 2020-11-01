f6476861f092c05d9b46ab889350357e2aff1213
pagidas <pagakrivos@gmail.com>
2020-11-01 17:50:31 +0000

    build(script): adds option to re download new images

--- spin_all.sh
+++ spin_all.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-if [ "$1" == 'build' ]; then
+if [ "$1" == 'build' ]
+then
   echo "Building images and running all containers..."
   docker-compose up -d --build
+elif [ "$1" == 'fetch-new' ]
+then
+  echo "Removing old containers..."
+  docker rmi $(docker images | grep ghcr.io/pagidas)
+  echo "Downloading new containers..."
+  docker-compose up -d --build
 else
-  echo "Running all containers from previously built images..."
+  echo "Spinning up containers..."
   docker-compose up -d
 fi
 echo "Done! All services running in the background
