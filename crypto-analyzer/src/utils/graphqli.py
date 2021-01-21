ec019c4b3a9e98bb145464955ead0676d9d38cf3
Nikos Giachoudis <ngiachou@gmail.com>
2021-01-21 20:51:46 +0200

    cleans unused code

--- crypto-analyzer/src/utils/graphqli.py
+++ crypto-analyzer/src/utils/graphqli.py
@@ -15,35 +15,3 @@ class GraphqlClient:
             'Content-Type': 'application/json'
         }
         return requests.request(method, url, headers=headers, data=payload)
-
-    def get_subs(self, endpoint, method):
-        """
-        Speaks with graphql service to receive all current subscriptions
-        :param endpoint: the endpoint for getting current subscriptions
-        :param method: the method of the request
-        :return: http response.
-        """
-        url = self.base + endpoint
-        payload = '{"query":"{\\n    subscriptions\\n}","variables":{}}'
-        headers = {
-            'Content-Type': 'application/json'
-        }
-        return requests.request(method, url, headers=headers, data=payload)
-
-    def get_most_recent_messages(self, endpoint, method, number=100):
-        """
-        In this function we request from graphql service to fetch the most
-        recent messages from rethinkdb
-        :param endpoint: the endpoint for getting recent messages from graphql server
-        :param method: the method of the request
-        :param number: is the number of most recent messages (default is 100)
-        :return: http response.
-        """
-        url = self.base + endpoint
-        payload = "{{\"query\":\"{{\\n  messages(mostRecent: {}) {{\\n    productId\\n    price\\n    time\\n  " \
-                  "}}\\n}}\",\"variables\":{{}} }}".format(number)
-        headers = {
-            'Content-Type': 'application/json'
-        }
-
-        return requests.request(method, url, headers=headers, data=payload)
