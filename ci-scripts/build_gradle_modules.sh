1261ccb2604d2230262459bde246d0be43c95283
pagidas <pagakrivos@gmail.com>
2021-02-20 23:39:23 +0000

    Test action to execute on specific module

--- ci-scripts/build_gradle_modules.sh
+++ ci-scripts/build_gradle_modules.sh
@@ -4,30 +4,22 @@
 BASE_DIR=$(pwd)
 
 # arg$1: the folder where the repo exists
-build_if_exists() {
-  local folder=$1
+folder=$1
 
-  if [[ -d "../$folder" ]]; then
-    echo "$folder gradle module is present"
+if [[ -d "../$folder" ]]; then
+  echo "$folder gradle module is present"
 
-    if [ "$folder" == 'http4k-starter' ]; then
-      # First build and publish to local maven http4k-starter
-      cd ../http4k-starter
-      ./gradlew clean build publishToMavenLocal
-      cd "$BASE_DIR"
-      return 1
-    fi
-
-    cd ../"$folder"
-    echo "Building $folder..."
-    # we only care if they compile, thus skip building the fatjar
-    ./gradlew clean build -x shadowJar -x jar
+  if [ "$folder" == 'http4k-starter' ]; then
+    # First build and publish to local maven http4k-starter
+    cd ../http4k-starter
+    ./gradlew clean build publishToMavenLocal
     cd "$BASE_DIR"
+    return 1
   fi
-}
 
-
-## main script
-build_if_exists "http4k-starter"
-build_if_exists "coinbase-integration"
-build_if_exists "graphql"
+  cd ../"$folder"
+  echo "Building $folder..."
+  # we only care if they compile, thus skip building the fatjar
+  ./gradlew clean build -x shadowJar -x jar
+  cd "$BASE_DIR"
+fi
--- coinbase-integration/subscriber/src/main/kotlin/me/pysquad/cryptobot/subscriber/coinbase/model/CoinbaseWsSubscribeRequest.kt
+++ coinbase-integration/subscriber/src/main/kotlin/me/pysquad/cryptobot/subscriber/coinbase/model/CoinbaseWsSubscribeRequest.kt
@@ -1,10 +1,10 @@
 package me.pysquad.cryptobot.subscriber.coinbase.model
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import me.pysquad.cryptobot.subscriber.model.ProductIds
 
-@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class CoinbaseWsSubscribeRequest(
         val type: String,
         val productIds: ProductIds,
