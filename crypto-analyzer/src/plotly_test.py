2311971b42ca424dd12e380922c9b2463b6923c2
esigalas <evsigalas@gmail.com>
2021-02-24 00:16:44 +0200

    Added an inital script test with plotly and dash that updates a plot live. Need to see how to incorporate it in our code and also figure out if that is the optimal way to udpate.

---
+++ crypto-analyzer/src/plotly_test.py
@@ -0,0 +1,81 @@
+import plotly.graph_objects as go
+
+import pandas as pd
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Load data
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv")
+df.columns = [col.replace("AAPL.", "") for col in df.columns]
+
+# Create figure
+fig = go.Figure()
+
+fig.add_trace(
+    go.Scatter(x=list(df.Date), y=list(df.High)))
+
+# Set title
+fig.update_layout(
+    title_text="Time series with range slider and selectors"
+)
+
+# Add range slider
+fig.update_layout(
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label="1m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=6,
+                     label="6m",
+                     step="month",
+                     stepmode="backward"),
+                dict(count=1,
+                     label="YTD",
+                     step="year",
+                     stepmode="todate"),
+                dict(count=1,
+                     label="1y",
+                     step="year",
+                     stepmode="backward"),
+                dict(step="all")
+            ])
+        ),
+        rangeslider=dict(
+            visible=True
+        ),
+        type="date"
+    )
+)
+
+app = dash.Dash()
+app.layout = html.Div([
+    dcc.Graph(figure=fig, id='live-update-graph'),
+    dcc.Interval(
+            id='interval-component',
+            interval=5*1000, # in milliseconds
+            n_intervals=0
+    )
+])
+
+
+@app.callback(Output('live-update-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_graph_live(n):
+    # print("T")
+    # with fig.batch_update():
+    #     fig.data[0].y = fig.data[0].y
+    fig.update_layout(
+        title_text=fig['layout'].title.text+"a"
+    )
+    fig.update(data=[{'y': [4, 5, 6]}])
+    return fig
+# fig.show()
+
+app.run_server(debug=True, use_reloader=False)  # Turn off reloader if inside Jupyter
