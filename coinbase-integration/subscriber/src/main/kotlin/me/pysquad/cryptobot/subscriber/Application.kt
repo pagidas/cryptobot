09d702b07019a5ea10eacd8d70403aff2a6b7447
pagidas <pagakrivos@gmail.com>
2021-05-17 19:14:39 +0100

    Configurable application according to config easy for test

--- coinbase-integration/subscriber/src/main/kotlin/me/pysquad/cryptobot/subscriber/Application.kt
+++ coinbase-integration/subscriber/src/main/kotlin/me/pysquad/cryptobot/subscriber/Application.kt
@@ -4,27 +4,22 @@ import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import me.pysquad.cryptobot.http4k.starter.http4kApp
 
-class Application {
-    companion object {
-        fun start(config: Config) {
-            val appConfig = AppConfiguration(config)
-            val coinbaseConfig = CoinbaseConfiguration(config)
-            val rethinkDbConfig = RethinkDbConfiguration(config)
+class Application(config: Config) {
+    val appConfig = AppConfiguration(config)
+    val coinbaseConfig = CoinbaseConfiguration(config)
+    val rethinkDbConfig = RethinkDbConfiguration(config)
 
-            http4kApp(appConfig.port) {
-                // building the service
-                val subscriberService = SubscriberService(
-                    CoinbaseWsFeedNonBlocking(coinbaseConfig),
-                    SubscriberRepository.impl(RethinkDbDatasource(rethinkDbConfig))
-                )
+    val subscriberService = SubscriberService(
+        CoinbaseWsFeedNonBlocking(coinbaseConfig),
+        SubscriberRepository.impl(RethinkDbDatasource(rethinkDbConfig))
+    )
 
-                // passing the routes
-                SubscriberWebController(subscriberService).routes()
-            }.run()
-        }
-    }
+    fun start() = http4kApp(appConfig.port) {
+        // passing the routes
+        SubscriberWebController(subscriberService).routes()
+    }.run()
 }
 
 fun main() {
-    Application.start(ConfigFactory.load())
+    Application(ConfigFactory.load()).start()
 }
