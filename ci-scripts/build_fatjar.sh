54a8828ecfbbca9cbb5c6aa1426e0e4765f84b3e
pagidas <pagakrivos@gmail.com>
2021-05-19 22:17:14 +0100

    Fixes ci script to build jar before image publish

--- ci-scripts/build_fatjar.sh
+++ ci-scripts/build_fatjar.sh
@@ -13,19 +13,19 @@ if [[ -d "../$folder" ]]; then
     # First build and publish to local maven http4k-starter.
     echo "Building and publish to maven local http4k-starter..."
     cd ../http4k-starter
-    ./gradlew clean build publishToMavenLocal
+    ./gradlew clean build publishToMavenLocal -x test
     cd "$BASE_DIR"
 
     # Build the target module dependent on http4k-starter.
     cd ../"$folder"
     echo "Building $folder..."
-    ./gradlew clean build
+    ./gradlew clean build shadowJar -x test
     cd "$BASE_DIR"
     return 1
   fi
 
   cd ../"$folder"
   echo "Building $folder..."
-  ./gradlew clean build
+  ./gradlew clean build shadowJar -x test
   cd "$BASE_DIR"
 fi
