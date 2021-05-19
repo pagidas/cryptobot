bbeb9a1156c5e9b4cd4382ed3d9999a44280d809
pagidas <pagakrivos@gmail.com>
2021-05-19 21:54:43 +0100

    Moves container specific declarations in a base

---
+++ coinbase-integration/subscriber/src/test/kotlin/me/pysquad/cryptobot/subscriber/AbstractContainerBase.kt
@@ -0,0 +1,18 @@
+package me.pysquad.cryptobot.subscriber
+
+import org.testcontainers.containers.GenericContainer
+import org.testcontainers.junit.jupiter.Container
+
+abstract class AbstractContainerBase {
+    companion object {
+        private const val containerRethinkDbPort = 28015
+
+        @Container
+        @JvmStatic
+        val containerRethinkDb = GenericContainer<Nothing>("rethinkdb:2.4.0").apply {
+            withExposedPorts(containerRethinkDbPort)
+        }
+
+        val exposedContainerRethinkDbPort: Int by lazy { containerRethinkDb.getMappedPort(containerRethinkDbPort) }
+    }
+}
